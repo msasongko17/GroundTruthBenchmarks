8aebb1999123ae9097e7de5dcf1cccf8937443f9
aditya <msasongko17@ku.edu.tr>
2020-08-07 23:24:08 +0300

    adding LocDetectiveBench

---
+++ LocDetectiveBench/long_rd/multithread_five_rd_inc_long.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <omp.h>
+
+// bin start size: 130
+int main () {
+	int val = 0;
+	int num_threads = 0;
+	/*__asm__ __volatile__ ("movl $100000000, %%ecx\n\t"
+                "movl $0, %%edx\n\t"
+                "1:\n\t"
+                "incl %0\n\t"
+                "loop 1b\n\t"
+		: "=m" (val)
+		:
+		: "memory", "%ecx", "%edx"
+            );*/
+#pragma omp parallel
+	{
+	
+	char array1[400000];
+	// time distance: 200000, frequency: 2 M 
+	__asm__ __volatile__ ("movl $10, %%edx\n\t"
+		"loop1:\n\t"
+		"movl $200000, %%eax\n\t"
+		"movq %%rcx, %%rbx\n\t"
+		"loop:\n\t"
+		"movl %%edx, (%%rbx)\n\t"
+		"addq $1, %%rbx\n\t"
+		"decl %%eax\n\t"
+		"jnz loop\n\t"
+		"decl %%edx\n\t"
+		"jnz loop1\n\t"
+                : 
+                : "c" (array1)
+                : "%edx", "%eax", "memory", "cc"
+            );
+	// time distance: 500000, frequency: 5 M 
+	__asm__ __volatile__ ("movl $10, %%edx\n\t"
+		"loop3:\n\t"
+		"movl $500000, %%eax\n\t"
+		"movq %%rcx, %%rbx\n\t"
+		"loop2:\n\t"
+		"movl %%edx, (%%rbx)\n\t"
+		"addq $1, %%rbx\n\t"
+		"decl %%eax\n\t"
+		"jnz loop2\n\t"
+		"decl %%edx\n\t"
+		"jnz loop3\n\t"
+                : 
+                : "c" (array1)
+                : "%edx", "%eax", "memory", "cc"
+            );	
+	// time distance: 1000000, frequency: 10 M 
+	__asm__ __volatile__ ("movl $10, %%edx\n\t"
+		"loop5:\n\t"
+		"movl $1000000, %%eax\n\t"
+		"movq %%rcx, %%rbx\n\t"
+		"loop4:\n\t"
+		"movl %%edx, (%%rbx)\n\t"
+		"addq $1, %%rbx\n\t"
+		"decl %%eax\n\t"
+		"jnz loop4\n\t"
+		"decl %%edx\n\t"
+		"jnz loop5\n\t"
+                : 
+                : "c" (array1)
+                : "%edx", "%eax", "memory", "cc"
+            );
+	// time distance: 2000000, frequency: 20 M 
+	__asm__ __volatile__ ("movl $10, %%edx\n\t"
+		"loop7:\n\t"
+		"movl $2000000, %%eax\n\t"
+		"movq %%rcx, %%rbx\n\t"
+		"loop6:\n\t"
+		"movl %%edx, (%%rbx)\n\t"
+		"addq $1, %%rbx\n\t"
+		"decl %%eax\n\t"
+		"jnz loop6\n\t"
+		"decl %%edx\n\t"
+		"jnz loop7\n\t"
+                : 
+                : "c" (array1)
+                : "%edx", "%eax", "memory", "cc"
+            );
+        // time distance: 4000000, frequency: 40 M 
+	__asm__ __volatile__ ("movl $10, %%edx\n\t"
+		"loop9:\n\t"
+		"movl $4000000, %%eax\n\t"
+		"movq %%rcx, %%rbx\n\t"
+		"loop8:\n\t"
+		"movl %%edx, (%%rbx)\n\t"
+		"addq $1, %%rbx\n\t"
+		"decl %%eax\n\t"
+		"jnz loop8\n\t"
+		"decl %%edx\n\t"
+		"jnz loop9\n\t"
+                : 
+                : "c" (array1)
+                : "%edx", "%eax", "memory", "cc"
+            );    
+	#pragma omp single
+	num_threads = omp_get_num_threads();
+	}
+	printf("RD: 200000, expected frequency %d M, RD: 500000, expected frequency %d M, RD: 1000000, expected frequency %d M, RD: 2000000, expected frequency %d M, RD: 4000000, expected frequency %d M\n", 2 * num_threads, 5 * num_threads, 10 * num_threads, 20 * num_threads, 40 * num_threads);
+	return 0;
+}
---
+++ LocDetectiveBench/short_rd/multithread_five_rd_bell.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <omp.h>
+
+// bin start size: 130
+int main () {
+	int val = 0;
+	int num_threads = 0;
+	/*__asm__ __volatile__ ("movl $100000000, %%ecx\n\t"
+                "movl $0, %%edx\n\t"
+                "1:\n\t"
+                "incl %0\n\t"
+                "loop 1b\n\t"
+		: "=m" (val)
+		:
+		: "memory", "%ecx", "%edx"
+            );*/
+#pragma omp parallel
+	{
+	int array1[50];
+	int array2[100];
+	int array3[300];
+	int array4[600];
+	int array5[1500];
+	// time distance: 50, frequency: 30 M 
+	__asm__ __volatile__ ("movl $600000, %%edx\n\t"
+		"loop1:\n\t"
+		"movl $50, %%eax\n\t"
+		"movq %%rcx, %%rbx\n\t"
+		"loop:\n\t"
+		"movl %%edx, (%%rbx)\n\t"
+		"addq $4, %%rbx\n\t"
+		"decl %%eax\n\t"
+		"jnz loop\n\t"
+		"decl %%edx\n\t"
+		"jnz loop1\n\t"
+                : 
+                : "c" (array1)
+                : "%edx", "%eax", "memory", "cc"
+            );
+        // time distance: 100, frequency: 60 M 
+	__asm__ __volatile__ ("movl$600000, %%edx\n\t"
+                "loop3:\n\t"
+                "movl $100, %%eax\n\t"
+                "movq %%rcx, %%rbx\n\t"
+                "loop2:\n\t"
+                "movl %%edx, (%%rbx)\n\t"
+                "addq $4, %%rbx\n\t"
+                "decl %%eax\n\t"
+                "jnz loop2\n\t"
+                "decl %%edx\n\t"
+                "jnz loop3\n\t"
+                :
+                : "c" (array2)
+                : "%edx", "%eax", "memory", "cc"
+            );	
+        // time distance: 300, frequency: 150 M 
+	__asm__ __volatile__ ("movl $500000, %%edx\n\t"
+                "loop4:\n\t"
+                "movl $300, %%eax\n\t"
+                "movq %%rcx, %%rbx\n\t"
+                "loop5:\n\t"
+                "movl %%edx, (%%rbx)\n\t"
+                "addq $4, %%rbx\n\t"
+                "decl %%eax\n\t"
+                "jnz loop5\n\t"
+                "decl %%edx\n\t"
+                "jnz loop4\n\t"
+                :
+                : "c" (array3)
+                : "%edx", "%eax", "memory", "cc"
+            );
+        // time distance: 600, frequency: 60 M
+	__asm__ __volatile__ ("movl $100000, %%edx\n\t"
+                "loop6:\n\t"
+                "movl $600, %%eax\n\t"
+                "movq %%rcx, %%rbx\n\t"
+                "loop7:\n\t"
+                "movl %%edx, (%%rbx)\n\t"
+                "addq $4, %%rbx\n\t"
+                "decl %%eax\n\t"
+                "jnz loop7\n\t"
+                "decl %%edx\n\t"
+                "jnz loop6\n\t"
+                :
+                : "c" (array4)
+                : "%edx", "%eax", "memory", "cc"
+            );
+        // time distance: 1500, frequency: 30 M
+        __asm__ __volatile__ ("movl $20000, %%edx\n\t"
+                "loop8:\n\t"
+                "movl $1500, %%eax\n\t"
+                "movq %%rcx, %%rbx\n\t"
+                "loop9:\n\t"
+                "movl %%edx, (%%rbx)\n\t"
+                "addq $4, %%rbx\n\t"
+                "decl %%eax\n\t"
+                "jnz loop9\n\t"
+                "decl %%edx\n\t"
+                "jnz loop8\n\t"
+                :
+                : "c" (array4)
+                : "%edx", "%eax", "memory", "cc"
+            );
+	#pragma omp single
+	num_threads = omp_get_num_threads();
+	}
+	printf("RD: 49, expected frequency %d M, RD: 99, expected frequency %d M, RD: 299, expected frequency %d M, RD: 599, expected frequency %d M, RD: 1499, expected frequency %d M\n", 30 * num_threads, 60 * num_threads, 150 * num_threads, 60 * num_threads, 30 * num_threads);
+	return 0;
+}
